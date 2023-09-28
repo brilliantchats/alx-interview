ac2d94f4fc6adb1aac9854fe7a56d0a1f62b4be3
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-09-28 19:58:15 +0000

    fix if first row has land

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,16 +9,23 @@ def island_perimeter(grid):
     perimeter = 0
     row = len(grid)
     col = len(grid[0])
-    assert (1 <= row and col <= 100), "Check conditions of size of grid"
     for i in range(row):
         for j in range(col):
             if grid[i][j] == 1:
-                if i - 1 >= 0 and grid[i - 1][j] == 0:
+                if i - 1 < 0:
                     perimeter += 1
-                if i + 1 <= row - 1 and grid[i + 1][j] == 0:
+                elif grid[i - 1][j] == 0:
                     perimeter += 1
-                if j - 1 >= 0 and grid[i][j - 1] == 0:
+                if i + 1 >= row:
                     perimeter += 1
-                if j + 1 <= col - 1 and grid[i][j + 1] == 0:
+                elif grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j - 1 < 0:
+                    perimeter += 1
+                elif grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j + 1 >= col:
+                    perimeter += 1
+                elif grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
